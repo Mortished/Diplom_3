915dcefb6dcd89aeb7ddcd38f522508a3b859e9a
s.kukhtanov <s.kukhtanov@tinkoff.ru>
2022-04-21 18:24:41 +0300

    review fix

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,23 +1,31 @@
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.url;
+import static io.restassured.RestAssured.given;
 
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import page.RegistrationPage;
+import pojo.LoginForm;
 
 @Story("Регистрация пользователя")
 public class RegistrationTest {
+
+    private final String email = new DataGenerator().getRandomEmail();
+    boolean isPositive;
 
     @Test
     @DisplayName("Регистрация пользователя")
     public void registerUser() {
         RegistrationPage registrationPage = open("https://stellarburgers.nomoreparties.site/register",
             RegistrationPage.class);
-        registrationPage.registerNewUser("Test", new DataGenerator().getRandomEmail(),"Test12");
+        registrationPage.registerNewUser("Test", email, Dictionary.PASSWORD);
         registrationPage.waitForLoadingLoginPage();
         Assert.assertEquals("Переход на форму логина не произведен", Dictionary.LOGIN_URL, url());
+        isPositive = true;
     }
 
     @Test
@@ -25,7 +33,28 @@ public class RegistrationTest {
     public void registerUserNegative() {
         RegistrationPage registrationPage = open("https://stellarburgers.nomoreparties.site/register",
             RegistrationPage.class);
-        registrationPage.registerNewUser("Test", new DataGenerator().getRandomEmail(),"Test");
+        registrationPage.registerNewUser("Test", email,"Test");
         Assert.assertTrue("Отсутствует предупреждение системы", registrationPage.isWarningVisible());
+    }
+
+    @After
+    public void cleanUp() {
+        if (isPositive) {
+            ValidatableResponse response = given()
+                .header("Content-type", "application/json")
+                .body(new LoginForm(email, Dictionary.PASSWORD))
+                .when()
+                .post("https://stellarburgers.nomoreparties.site/api/auth/login")
+                .then();
+
+            String token = response.extract().path("accessToken");
+
+            given()
+                .headers("Authorization", token,
+                    "Content-type", "application/json")
+                .when()
+                .delete("https://stellarburgers.nomoreparties.site/api/auth/user")
+                .then();
+        }
     }
 }
